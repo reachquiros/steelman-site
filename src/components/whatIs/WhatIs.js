53d4a1bf07934760b72314853aba6f89d46cf2a5
Charles Quiros <charles.quiro@gmail.com>
2022-07-09 19:47:19 -0700

    added img and top of page for what-is page

--- src/components/whatIs/WhatIs.js
+++ src/components/whatIs/WhatIs.js
@@ -1,9 +1,22 @@
 import React from "react";
+import styles from "./WhatIs.module.css";
 
 const WhatIs = () => {
   return (
-    <div>
-      <p>what-is</p>
+    <div className="container-fluid p-0">
+      <div className="text-center d-flex row align-items-center">
+        <img
+          src="https://picsum.photos/3200/1800?grayscale"
+          alt=""
+          className="img-fluid"
+        />
+        <h1
+          className={`position-absolute text-center mt-4 display-5 ${styles.h1}`}
+        >
+          Steelman Society is about being the change we want to see in the
+          world.
+        </h1>
+      </div>
     </div>
   );
 };
