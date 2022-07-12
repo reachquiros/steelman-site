1b21a330922d480ec251e5a57360435a0a462584
Charles Quiros <charles.quiro@gmail.com>
2022-07-11 18:03:00 -0700

    chose fixed images for home page and set proper img scrolling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Steelman Website.
 
 Libraries/Frameworks:
-React, React Router, React Hooks, Boostrap React, Mdbootstrap, Font Awesome, CSS Modules.
+React, React Router, React Hooks, Boostrap React, Mdbootstrap, AOS Scrolling, Font Awesome, CSS Modules.
 
 Will be incorporating: Typescript, NextJS, Firebase.
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import home_front from "../../assets/images/home_front.jpg";
+import mid_elem_leaf from "../../assets/images/mid_elem_leaf.jpg";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import styles from "./Home.module.css";
@@ -11,30 +13,29 @@ const Home = (props) => {
   return (
     <div className={` ${styles.body} `}>
       <div className="container-fluid p-0">
-        <img
-          src="https://picsum.photos/2800/1200"
-          alt=""
-          className="img-fluid shadow-sm"
-        />
+        <img src={home_front} alt="" className="img-fluid shadow-sm" />
       </div>
-      <div data-aos="fade-up" className="my-5 mx-2">
-        <div className="p-5 m-5 text-center">
-          <h1 className={`${styles.title}`}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing.
-          </h1>
+      <div data-aos="fade-up">
+        <div className="my-5 mx-2">
+          <div className="p-5 m-5 text-center">
+            <h1 className={`${styles.title}`}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+            </h1>
 
-          <p className={`${styles.subtitle}`}>
-            Duis ut diam quam nulla porttitor massa id neque.
-          </p>
+            <p className={`${styles.subtitle}`}>
+              Duis ut diam quam nulla porttitor massa id neque.
+            </p>
+          </div>
+        </div>
+        <div className="container-fluid p-0">
+          <img
+            src={mid_elem_leaf}
+            alt=""
+            className={`img-fluid shadow-sm ${styles.fadeInImage}`}
+          />
         </div>
       </div>
-      <div className="container-fluid p-0">
-        <img
-          src="https://picsum.photos/2800/800"
-          alt=""
-          className={`img-fluid shadow-sm ${styles.fadeInImage}`}
-        />
-      </div>
+
       <div className="container m-5 mx-auto p-5">
         <Card />
         <Card />
