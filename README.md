7ecd92af8ff1bbf170cb312207090ad3e1105129
Charles Quiros <charles.quiro@gmail.com>
2022-07-13 11:58:29 -0700

    working on the events page. Added containers to consider style and responsiveness

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Steelman Website.
 
 Libraries/Frameworks:
-React, React Router, React Hooks, Boostrap React, Mdbootstrap, AOS Scrolling, Font Awesome, CSS Modules.
+React, React Router, React Hooks, Boostrap React, Mdbootstrap, AOS Scrolling, Font Awesome, Node Sass, CSS Modules.
 
 Will be incorporating: Typescript, NextJS, Firebase.
--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -4,12 +4,51 @@ import "./Events.css";
 const Events = () => {
   return (
     <div>
-      <div className="image-grid">
-        <img src="https://picsum.photos/200/300" alt="" />
-        <img src="https://picsum.photos/200/300" alt="" />
-        <img src="https://picsum.photos/200/300" alt="" />
-        <img src="https://picsum.photos/200/300" alt="" />
-        <img src="https://picsum.photos/200/300" alt="" />
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-6 mb-2">
+            <div className="p-5 bg-primary"></div>
+          </div>
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-secondary"></div>
+          </div>
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-info"></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-danger"></div>
+          </div>
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-success"></div>
+          </div>
+          <div className="col-6 mb-2">
+            <div className="p-5 bg-danger"></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6 mb-2">
+            <div className="p-5 bg-warning"></div>
+          </div>
+          <div className="col-6 mb-2">
+            <div className="p-5 bg-secondary"></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-primary"></div>
+          </div>
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-danger"></div>
+          </div>
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-info"></div>
+          </div>
+          <div className="col-3 mb-2">
+            <div className="p-5 bg-success"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
