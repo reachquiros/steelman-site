bbe7a326ae1a423985bace07de1a49d7a66a88aa
Charles Quiros <charles.quiro@gmail.com>
2022-07-14 22:03:00 -0700

    added form to contact page and made more dark mode adjustments.

--- src/components/contact-us/ContactUs.js
+++ src/components/contact-us/ContactUs.js
@@ -1,7 +1,183 @@
 import React from "react";
+import styles from "./ContactUs.module.css";
+import "bootstrap/js/src/collapse.js";
 
 const ContactUs = () => {
-  return <div>contact-us</div>;
+  return (
+    <div>
+      <section className={`intro ${styles.intro}`}>
+        <div className="mask d-flex align-items-center h-100">
+          <div className="container">
+            <div className="row justify-content-center mt-5">
+              <div className={`col-12 col-lg-10 col-xl-10 `}>
+                <div className={`card ${styles.card} `}>
+                  <div className="card-body m-3 p-4 p-md-5">
+                    <h3 className="mb-4 pb-2">Reach Out</h3>
+
+                    <form action="">
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="firstName"
+                              className="form-control"
+                            />
+                            <label className="form-label" htmlFor="firstName">
+                              First Name
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="lastName"
+                              className="form-control"
+                            />
+                            <label className="form-label" htmlFor="lastName">
+                              Last Name
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline datepicker">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="birthdayDate"
+                            />
+                            <label
+                              htmlFor="birthdayDate"
+                              className="form-label"
+                            >
+                              Birthday
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                          <h6 className="mb-2 pb-1">Gender: </h6>
+
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="inlineRadioOptions"
+                              id="femaleGender"
+                              value="option1"
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="femaleGender"
+                            >
+                              Female
+                            </label>
+                          </div>
+
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="inlineRadioOptions"
+                              id="maleGender"
+                              value="option2"
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="maleGender"
+                            >
+                              Male
+                            </label>
+                          </div>
+
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="inlineRadioOptions"
+                              id="otherGender"
+                              value="option3"
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="otherGender"
+                            >
+                              Other
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="email"
+                              id="emailAddress"
+                              className="form-control"
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="emailAddress"
+                            >
+                              Email
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="tel"
+                              id="phoneNumber"
+                              className="form-control"
+                            />
+                            <label className="form-label" htmlFor="phoneNumber">
+                              Phone Number
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-12">
+                          <h6 className="mb-3">Subject</h6>
+
+                          <select className="select" multiple>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                            <option value="4">Four</option>
+                            <option value="5">Five</option>
+                            <option value="6">Six</option>
+                            <option value="7">Seven</option>
+                            <option value="8">Eight</option>
+                          </select>
+                          <label className="form-label select-label">
+                            Choose option
+                          </label>
+
+                          <div className="mt-4">
+                            <input
+                              className="btn btn-warning btn-lg"
+                              type="submit"
+                              value="Submit"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ContactUs;
--- src/components/whatIs/WhatIs.js
+++ src/components/whatIs/WhatIs.js
@@ -24,39 +24,43 @@ const WhatIs = () => {
 
       <div data-aos="fade-up">
         <main className={` ${styles.main} `}>
-          <div className="container-lg rounded">
+          <div className={`container-lg rounded ${styles.mainContainer} `}>
             <div className="row justify-content-center my-5 py-5">
               <div className="col-md-5 text-center text-md-start">
-                <h1>
-                  <div className="display-5">Our Mission</div>
-                  <div className="display-6 text-muted">Forging the Self</div>
-                </h1>
-                <p className="lead my-4">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Esse, est laboriosam provident aut aliquam voluptas quod eum
-                  rerum laborum modi ad dolorum tenetur voluptates aspernatur ab
-                  consequuntur numquam nobis optio. Deleniti cupiditate nihil
-                  atque odio quia dignissimos. Voluptates quidem hic dolore nam
-                  a rerum maxime sunt illum blanditiis nobis eveniet,
-                  perspiciatis fugiat adipisci, placeat at ducimus voluptatum
-                  enim officiis facilis optio saepe temporibus voluptate!
-                  Delectus cum sed, laudantium corrupti tenetur laboriosam
-                  possimus, tempore perferendis animi distinctio, nam ratione
-                  neque illo eveniet id! Tempore eaque numquam officiis ratione.
-                  Non amet exercitationem minima praesentium, iste nobis modi
-                  nisi dignissimos ipsum architecto ducimus!
-                </p>
-                <p className="lead my-4">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Ipsum hic cupiditate explicabo, illo aliquam excepturi quas ab
-                  molestiae modi, debitis quaerat odit dolorum magni saepe
-                  delectus culpa inventore vel, impedit iste! Facere molestiae
-                  reiciendis exercitationem architecto nobis sint ducimus totam,
-                  expedita quod voluptatum eos dolore dignissimos atque commodi.
-                  Tempora suscipit quod facere vitae quaerat temporibus
-                  cupiditate veniam autem labore nostrum itaque minus accusamus
-                  sequi inventore, magni animi nobis voluptatibus modi!
-                </p>
+                <div className="title">
+                  <h2 className={`display-5 ${styles.h2} `}>Our Mission</h2>
+                  <h3 className={`display-6 ${styles.h3}`}>Forging the Self</h3>
+                </div>
+                <div className={`lead my-4 ${styles.p}`}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Esse, est laboriosam provident aut aliquam voluptas quod eum
+                    rerum laborum modi ad dolorum tenetur voluptates aspernatur
+                    ab consequuntur numquam nobis optio. Deleniti cupiditate
+                    nihil atque odio quia dignissimos. Voluptates quidem hic
+                    dolore nam a rerum maxime sunt illum blanditiis nobis
+                    eveniet, perspiciatis fugiat adipisci, placeat at ducimus
+                    voluptatum enim officiis facilis optio saepe temporibus
+                    voluptate! Delectus cum sed, laudantium corrupti tenetur
+                    laboriosam possimus, tempore perferendis animi distinctio,
+                    nam ratione neque illo eveniet id! Tempore eaque numquam
+                    officiis ratione. Non amet exercitationem minima
+                    praesentium, iste nobis modi nisi dignissimos ipsum
+                    architecto ducimus!
+                  </p>
+                  <p className="lead my-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Ipsum hic cupiditate explicabo, illo aliquam excepturi quas
+                    ab molestiae modi, debitis quaerat odit dolorum magni saepe
+                    delectus culpa inventore vel, impedit iste! Facere molestiae
+                    reiciendis exercitationem architecto nobis sint ducimus
+                    totam, expedita quod voluptatum eos dolore dignissimos atque
+                    commodi. Tempora suscipit quod facere vitae quaerat
+                    temporibus cupiditate veniam autem labore nostrum itaque
+                    minus accusamus sequi inventore, magni animi nobis
+                    voluptatibus modi!
+                  </p>
+                </div>
               </div>
               <div className="col-md-5 text-center">
                 <img className="img-fluid rounded" src={desktop} alt="" />
@@ -67,42 +71,45 @@ const WhatIs = () => {
       </div>
       <div data-aos="fade-up">
         <main className={` ${styles.main} `}>
-          <div className="container-lg  rounded">
+          <div className={`container-lg rounded ${styles.mainContainer} `}>
             <div className="row justify-content-center my-5 py-5">
               <div className="col-md-5 text-center text-md-start">
-                <h1>
-                  <div className="display-5">Our Method</div>
-                  <div className="display-6 text-muted">
+                <div className="title">
+                  <h2 className={`display-5 ${styles.h2} `}>Our Method</h2>
+                  <h3 className={`display-6 ${styles.h3}`}>
                     Growth Through Strife
-                  </div>
-                </h1>
-                <p className="lead my-4">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Ipsum hic cupiditate explicabo, illo aliquam excepturi quas ab
-                  molestiae modi, debitis quaerat odit dolorum magni saepe
-                  delectus culpa inventore vel, impedit iste! Facere molestiae
-                  reiciendis exercitationem architecto nobis sint ducimus totam,
-                  expedita quod voluptatum eos dolore dignissimos atque commodi.
-                  Tempora suscipit quod facere vitae quaerat temporibus
-                  cupiditate veniam autem labore nostrum itaque minus accusamus
-                  sequi inventore, magni animi nobis voluptatibus modi!
-                </p>
-                <p className="lead my-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                  culpa iste pariatur quisquam mollitia quia, perspiciatis
-                  reprehenderit iure repellat tenetur unde, eaque provident.
-                  Aperiam mollitia quaerat omnis beatae corrupti doloribus,
-                  itaque dolor aliquid obcaecati sint recusandae nam nulla,
-                  dolorem distinctio, dolore minima laboriosam adipisci
-                  inventore nemo harum assumenda accusamus veniam officia!
-                  Perspiciatis, voluptatibus. Saepe hic soluta vel, esse, alias
-                  earum in sunt ut deserunt possimus fugiat. Pariatur molestias
-                  corrupti doloremque porro officiis aliquid, nobis temporibus
-                  quis hic ab tenetur voluptate eligendi libero quos mollitia
-                  similique commodi. Saepe, obcaecati quibusdam inventore
-                  similique ducimus qui possimus maiores ipsa! Ut sit doloribus
-                  amet.
-                </p>
+                  </h3>
+                </div>
+                <div className={`lead my-4 ${styles.p}`}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Esse, est laboriosam provident aut aliquam voluptas quod eum
+                    rerum laborum modi ad dolorum tenetur voluptates aspernatur
+                    ab consequuntur numquam nobis optio. Deleniti cupiditate
+                    nihil atque odio quia dignissimos. Voluptates quidem hic
+                    dolore nam a rerum maxime sunt illum blanditiis nobis
+                    eveniet, perspiciatis fugiat adipisci, placeat at ducimus
+                    voluptatum enim officiis facilis optio saepe temporibus
+                    voluptate! Delectus cum sed, laudantium corrupti tenetur
+                    laboriosam possimus, tempore perferendis animi distinctio,
+                    nam ratione neque illo eveniet id! Tempore eaque numquam
+                    officiis ratione. Non amet exercitationem minima
+                    praesentium, iste nobis modi nisi dignissimos ipsum
+                    architecto ducimus!
+                  </p>
+                  <p className="lead my-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Ipsum hic cupiditate explicabo, illo aliquam excepturi quas
+                    ab molestiae modi, debitis quaerat odit dolorum magni saepe
+                    delectus culpa inventore vel, impedit iste! Facere molestiae
+                    reiciendis exercitationem architecto nobis sint ducimus
+                    totam, expedita quod voluptatum eos dolore dignissimos atque
+                    commodi. Tempora suscipit quod facere vitae quaerat
+                    temporibus cupiditate veniam autem labore nostrum itaque
+                    minus accusamus sequi inventore, magni animi nobis
+                    voluptatibus modi!
+                  </p>
+                </div>
               </div>
               <div className="col-md-5 text-center">
                 <img className="img-fluid rounded" src={rows} alt="" />
