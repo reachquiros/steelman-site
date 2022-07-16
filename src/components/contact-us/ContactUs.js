d627e8c18403f306a9a0076f69089db0d4aca5dc
Charles Quiros <charles.quiro@gmail.com>
2022-07-15 17:35:29 -0700

    added styling to the contact-us page

--- src/components/contact-us/ContactUs.js
+++ src/components/contact-us/ContactUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ContactUs.module.css";
-import "bootstrap/js/src/collapse.js";
+import * as mdb from "mdb-ui-kit";
+import { Input } from "mdb-ui-kit";
 
 const ContactUs = () => {
   return (
@@ -12,7 +13,9 @@ const ContactUs = () => {
               <div className={`col-12 col-lg-10 col-xl-10 `}>
                 <div className={`card ${styles.card} `}>
                   <div className="card-body m-3 p-4 p-md-5">
-                    <h3 className="mb-4 pb-2">Reach Out</h3>
+                    <h1 className={`mb-4 display-5 pb-2 ${styles.title}`}>
+                      Reach Out
+                    </h1>
 
                     <form action="">
                       <div className="row">
@@ -63,7 +66,7 @@ const ContactUs = () => {
 
                           <div className="form-check form-check-inline">
                             <input
-                              className="form-check-input"
+                              className={`form-check-input  ${styles.radio}`}
                               type="radio"
                               name="inlineRadioOptions"
                               id="femaleGender"
@@ -143,25 +146,9 @@ const ContactUs = () => {
 
                       <div className="row">
                         <div className="col-12">
-                          <h6 className="mb-3">Subject</h6>
-
-                          <select className="select" multiple>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                            <option value="4">Four</option>
-                            <option value="5">Five</option>
-                            <option value="6">Six</option>
-                            <option value="7">Seven</option>
-                            <option value="8">Eight</option>
-                          </select>
-                          <label className="form-label select-label">
-                            Choose option
-                          </label>
-
                           <div className="mt-4">
                             <input
-                              className="btn btn-warning btn-lg"
+                              className={`btn btn-lg btn-block ${styles.submitButton}`}
                               type="submit"
                               value="Submit"
                             />
